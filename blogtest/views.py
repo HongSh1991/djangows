58401295650e1e169070589c882d3d5735545f7c
HongSh1991 <hjmrezl@outlook.com>
2018-02-19 23:25:22 +0800

    add template files

--- blogtest/views.py
+++ blogtest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import BlogArticles
+
 
 # Create your views here.
+def blog_title(request):
+	blogs = BlogArticles.objects.all()
+	return render(request, "blogtest/titles.html", {"blogs": blogs})
