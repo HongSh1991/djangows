6a2b6485920b8b1af6a7fdf437b13c1628813926
HongSh1991 <hjmrezl@outlook.com>
2018-02-20 23:13:03 +0800

    modify template files

--- blogtest/views.py
+++ blogtest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogArticles
 
 
@@ -6,3 +6,10 @@ from .models import BlogArticles
 def blog_title(request):
 	blogs = BlogArticles.objects.all()
 	return render(request, "blogtest/titles.html", {"blogs": blogs})
+
+
+def blog_article(request, article_id):
+	# article = BlogArticles.objects.get(id=article_id)
+	article = get_object_or_404(BlogArticles, id=article_id)
+	pub = article.publish
+	return render(request, "blogtest/content.html", {"article": article, "publish": pub})
