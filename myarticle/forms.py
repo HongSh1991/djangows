40bb7481448ae9479cf488d39cb0f0f489b70520
HongSh1991 <hjmrezl@outlook.com>
2018-02-21 20:17:43 +0800

    add form.py modify view.py

---
+++ myarticle/forms.py
@@ -0,0 +1,9 @@
+# -*-coding:utf8-*-
+from django import forms
+from .models import ArticleColumn
+
+
+class ArticleColumnForm(forms.ModelForm):
+	class Meta:
+		model = ArticleColumn
+		fields = ("column", )
